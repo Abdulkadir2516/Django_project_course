cf17e9db124179be6413ecfefcae1ab3f57da086
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2026-09-17 00:28:27 +0300

    Refactor index template to include categories and courses; add course partial template; update navigation link

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,4 +1,6 @@
 
+from datetime import date
+
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -10,16 +12,67 @@ data = {
     
 }
 
+db = {
+    "courses": [
+        {
+            "title": "Python Programlama",
+            "description": "Python programlama dili ile ilgili kurslar listeleniyor...",
+            "image": "https://sudo.ubuntu-tr.net/assets/post/pyhton-programlama-seri-i/pyhton-programlama.jpeg",
+            "slug": "python-programlama",
+            "date": date(2023, 1, 1),
+            "is_active": True
+        },
+        {
+            "title": "Java Programlama",
+            "description": "Java programlama dili ile ilgili kurslar listeleniyor...",
+            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo4kBug2N2HU9AGwL2YpEaGgwNHv0NlM6ABML60CwWYXvD8czZb-njwrL3&s=10",
+            "slug": "java-programlama",
+            "date": date(2023, 1, 1),
+            "is_active": True
+        },
+        {
+            "title": "C# Programlama",
+            "description": "C# programlama dili ile ilgili kurslar listeleniyor...",
+            "image": "https://www.kozmoslisesi.com/wp-content/uploads/2021/03/c-sharp-nedir.jpeg",
+            "slug": "csharp-programlama",
+            "date": date(2023, 1, 1),
+            "is_active": True
+        },
+        {
+            "title": "JavaScript Programlama",
+            "description": "JavaScript programlama dili ile ilgili kurslar listeleniyor...",
+            "image": "https://www.infoworld.com/wp-content/uploads/2025/09/2263137-0-59682900-1757000713-shutterstock_1361674454-100939444-orig.jpg?quality=50&strip=all&w=1024",
+            "slug": "javascript-programlama",
+            "date": date(2023, 1, 1),
+            "is_active": True
 
+        },
+        {
+            "title": "PHP Programlama",
+            "description": "PHP programlama dili ile ilgili kurslar listeleniyor...",
+            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7JU8ig4hAX3LKfxXMNgZxPlZG0vzIxdIrKtKuX3-MrQXOQE0t9j6XSdg&s=10",
+            "slug": "php-programlama",
+            "date": date(2023, 1, 1),
+            "is_active": True
+        }
+
+    ],
+    "categories": [
+        {"id":1, "name": "Programlama", "slug": "programlama"},
+        {"id":2, "name": "Mobil Uygulamalar", "slug": "mobil-uygulamalar"},
+        {"id":3, "name": "Web Geliştirme", "slug": "web-gelistirme"}
+    ]
+}
 # Create your views here.
 def index(request):
-    categories_list = list(data.keys())
 
+    kurslar = db["courses"]
+    kategori_listesi = db["categories"]
     
 
-
     return render(request, 'courses/index.html', {
-        "categories": categories_list
+        "categories": kategori_listesi,
+        "courses": kurslar
     })
 
 def kurslar(request):
